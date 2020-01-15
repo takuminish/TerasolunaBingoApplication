e7bc8372a7f48c8f2c08fb8cb6cf6e2b16f01fd7
takuminish <s15t253@stu.kagawa-u.ac.jp>
2020-01-15 22:36:24 +0900

    ログインEntityの作成

--- src/main/java/com/example/bingo/domain/model/User.java
+++ src/main/java/com/example/bingo/domain/model/User.java
@@ -3,6 +3,8 @@ package com.example.bingo.domain.model;
 import java.io.Serializable;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -16,6 +18,7 @@ public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int userId;
 	
 	private String userName;
