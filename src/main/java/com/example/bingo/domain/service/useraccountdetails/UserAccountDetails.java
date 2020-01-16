9ded0fcc275eb5b77d72033677f5636b2f34507b
takuminish <s15t253@stu.kagawa-u.ac.jp>
2020-01-16 20:30:54 +0900

    UserDetailsServiceの作成

--- src/main/java/com/example/bingo/domain/service/useraccountdetails/UserAccountDetails.java
+++ src/main/java/com/example/bingo/domain/service/useraccountdetails/UserAccountDetails.java
@@ -1,17 +1,17 @@
-package com.example.bingo.domain.service.userdetails;
+package com.example.bingo.domain.service.useraccountdetails;
 
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.User;
 
 import com.example.bingo.domain.model.UserAccount;
 
-public class UserDetails extends User {
+public class UserAccountDetails extends User {
 	
 	private static final long serialVersionUID = 1L;
 	
 	private final UserAccount userAccount;
 	
-	public UserDetails(UserAccount userAccount) {
+	public UserAccountDetails(UserAccount userAccount) {
 		super(userAccount.getUserName(), userAccount.getPassword(), 
 				AuthorityUtils.createAuthorityList("ROLE_USER"));
 		this.userAccount = userAccount;
---
+++ src/main/java/com/example/bingo/domain/service/useraccountdetails/UserAccountDetailsService.java
@@ -0,0 +1,31 @@
+package com.example.bingo.domain.service.useraccountdetails;
+
+import javax.inject.Inject;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.transaction.annotation.Transactional;
+import org.terasoluna.gfw.common.exception.ResourceNotFoundException;
+
+import com.example.bingo.domain.model.UserAccount;
+import com.example.bingo.domain.service.useraccount.UserAccountSharedService;
+
+public class UserAccountDetailsService implements UserDetailsService {
+
+	@Inject
+	UserAccountSharedService userAccountSharedService;
+	
+	@Transactional(readOnly=true)
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		
+		try {
+		    UserAccount userAccount = userAccountSharedService.findOne(username);
+		    return new UserAccountDetails(userAccount);
+		} catch(ResourceNotFoundException e) {
+			throw new UsernameNotFoundException("user NotFOund", e);
+		}
+	}
+
+}
