cd7fe3d655168147d35fb08e533414f4bfcdff2a
takuminish <s15t253@stu.kagawa-u.ac.jp>
2020-01-19 02:12:22 +0900

    カラムはNotNull

--- src/main/java/com/example/bingo/domain/model/UserAccount.java
+++ src/main/java/com/example/bingo/domain/model/UserAccount.java
@@ -7,6 +7,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 
 import lombok.Data;
 
@@ -21,8 +22,10 @@ public class UserAccount implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long userId;
 	
+	@NotNull
 	private String userName;
 	
+	@NotNull
 	private String password;
 		
 }
