04544c161abadb6615c4175482e8cbd3f31acee8
takuminish <s15t253@stu.kagawa-u.ac.jp>
2020-01-19 02:12:04 +0900

    カラムはNotNull

--- src/main/java/com/example/bingo/domain/model/BingoRoom.java
+++ src/main/java/com/example/bingo/domain/model/BingoRoom.java
@@ -10,6 +10,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 
 import lombok.Data;
 
@@ -24,21 +25,23 @@ public class BingoRoom implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long bingoRoomId;
 	
+	@NotNull
 	private String roomName;
 	
+	@NotNull
 	private boolean started;
 	
+	@NotNull
 	private boolean finished;
 	
+	@NotNull
 	private Date createdAt;
 	
+	@NotNull
 	private Date updatedAt;
 	
 	@OneToOne
 	@JoinColumn(name="createUserId", referencedColumnName = "userId")
 	private UserAccount createUserAccount;
-	
-	
-	
 	
 }
