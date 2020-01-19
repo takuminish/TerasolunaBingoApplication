90e54c83010458afd21e3885e9636651ff64eb30
takuminish <s15t253@stu.kagawa-u.ac.jp>
2020-01-19 18:19:06 +0900

    トランザクションの管理

--- src/main/java/com/example/bingo/domain/service/bingo/BingoServiceImpl.java
+++ src/main/java/com/example/bingo/domain/service/bingo/BingoServiceImpl.java
@@ -5,12 +5,14 @@ import java.util.List;
 import javax.inject.Inject;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.bingo.domain.model.Bingo;
 import com.example.bingo.domain.model.BingoRoom;
 import com.example.bingo.domain.repository.bingo.BingoRepository;
 
 @Service
+@Transactional
 public class BingoServiceImpl implements BingoService {
 
 	@Inject
@@ -22,6 +24,7 @@ public class BingoServiceImpl implements BingoService {
 		return bingo;
 	}
 
+	@Transactional(readOnly=true)
 	@Override
 	public List<Bingo> findAllByBingoRoom(BingoRoom bingoRoom) {
 		List<Bingo> bingoList = bingoRepository.findAllByBingoRoomBingoRoomId(bingoRoom.getBingoRoomId());
