5013b467db7d5b965020e5834da154e13167c590
takuminish <s15t253@stu.kagawa-u.ac.jp>
2020-01-20 21:57:30 +0900

    コメントを追加

--- src/main/java/com/example/bingo/app/bingoroom/BingoRoomForm.java
+++ src/main/java/com/example/bingo/app/bingoroom/BingoRoomForm.java
@@ -7,23 +7,50 @@ import javax.validation.constraints.Size;
 
 import lombok.Data;
 
+/**
+ * ビンゴルーム用Form
+ * 
+ * @author takuminv
+ *
+ */
 @Data
 public class BingoRoomForm implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    /**
+     * BingoRoom作成時のForm
+     * 
+     * @author takuminv
+     *
+     */
     public static interface BingoRoomCreate {
 
     };
 
+    /**
+     * BingoRoom更新時のForm
+     * 
+     * @author takuminv
+     *
+     */
     public static interface BingoRoomUpdate {
 
     }
 
+    /**
+     * BingoRoom削除時のForm
+     * 
+     * @author takuminv
+     *
+     */
     public static interface BingoRoomDelete {
 
     }
 
+    /**
+     * ルーム名
+     */
     @Size(min = 1, groups = { BingoRoomCreate.class, BingoRoomUpdate.class })
     @NotNull(groups = { BingoRoomCreate.class, BingoRoomUpdate.class })
     private String roomName;
