df03077047a7d20ef926dd3f7a8fad95c4b95a6a
takuminish <s15t253@stu.kagawa-u.ac.jp>
2020-01-20 22:15:15 +0900

    コメントを追加

--- src/main/java/com/example/bingo/domain/repository/bingo/BingoResultRepository.java
+++ src/main/java/com/example/bingo/domain/repository/bingo/BingoResultRepository.java
@@ -6,8 +6,20 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.bingo.domain.model.BingoResult;
 
+/**
+ * BingoResult Entity用 Repository
+ * 
+ * @author takuminv
+ *
+ */
 
 public interface BingoResultRepository extends JpaRepository<BingoResult, Long> {
 
+    /**
+     * 指定したBingoRoomに紐付く全てのBingoResultを取得
+     * 
+     * @param bingoRoomId
+     * @return 指定したBingoRoomに紐付く全てのBingoResult
+     */
     List<BingoResult> findAllByBingoRoomBingoRoomId(long bingoRoomId);
 }
