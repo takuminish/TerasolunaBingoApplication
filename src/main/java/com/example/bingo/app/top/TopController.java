affb9f4d6e036186731da24f3228f9bb40b5240c
takuminish <s15t253@stu.kagawa-u.ac.jp>
2020-01-20 22:01:46 +0900

    コメントを追加

--- src/main/java/com/example/bingo/app/top/TopController.java
+++ src/main/java/com/example/bingo/app/top/TopController.java
@@ -4,10 +4,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * トップ画面用 Controller
+ * 
+ * @author takuminv
+ *
+ */
 @Controller
 @RequestMapping("/")
 public class TopController {
 
+    /**
+     * トップ画面
+     * 
+     * @return
+     */
     @GetMapping
     public String index() {
 
