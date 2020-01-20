62caeb02521cbd4acd388c324c3a0d23c43741eb
takuminish <s15t253@stu.kagawa-u.ac.jp>
2020-01-20 22:16:27 +0900

    コメントを追加

--- src/main/java/com/example/bingo/domain/repository/useraccount/UserAccountRepository.java
+++ src/main/java/com/example/bingo/domain/repository/useraccount/UserAccountRepository.java
@@ -6,8 +6,20 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.bingo.domain.model.UserAccount;
 
+/**
+ * UserAccount Entity用 Repository
+ * 
+ * @author takuminv
+ *
+ */
 public interface UserAccountRepository extends JpaRepository<UserAccount, Long> {
 
+    /**
+     * ユーザ名に該当するUserAccountを取得する
+     * 
+     * @param userName
+     * @return
+     */
     Optional<UserAccount> findByUserName(String userName);
 
 }
