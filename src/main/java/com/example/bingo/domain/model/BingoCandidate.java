45df6edfd6e7ce31685ce9987272da80efbe015a
takuminish <s15t253@stu.kagawa-u.ac.jp>
2020-01-20 22:03:33 +0900

    コメントを追加

--- src/main/java/com/example/bingo/domain/model/BingoCandidate.java
+++ src/main/java/com/example/bingo/domain/model/BingoCandidate.java
@@ -2,13 +2,31 @@ package com.example.bingo.domain.model;
 
 import lombok.Data;
 
+/**
+ * ビンゴの抽選候補クラス
+ * 
+ * @author takuminv
+ *
+ */
 @Data
 public class BingoCandidate {
 
+    /**
+     * ビンゴの結果となる値(0~99)
+     */
     private String bingoValue;
 
+    /**
+     * すでに抽選結果として出ているか
+     */
     private boolean resulted;
 
+    /**
+     * コンストラクタ
+     * 
+     * @param bingoValue
+     * @param resulted
+     */
     public BingoCandidate(String bingoValue, boolean resulted) {
         super();
         this.bingoValue = bingoValue;
