7f168e45783d25b8a24e784cee1af95c29ffa292
takuminish <s15t253@stu.kagawa-u.ac.jp>
2020-01-20 21:43:21 +0900

    コメントを追加

--- src/main/java/com/example/bingo/app/bingo/BingoForm.java
+++ src/main/java/com/example/bingo/app/bingo/BingoForm.java
@@ -8,16 +8,28 @@ import javax.validation.constraints.NotNull;
 
 import lombok.Data;
 
+/**
+ * ビンゴゲーム画面用 Form
+ * 
+ * @author takuminv
+ *
+ */
 @Data
 public class BingoForm implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    /**
+     * 抽選結果の値(0~99)
+     */
     @Min(0)
     @Max(99)
     @NotNull
     private String bingoValue;
 
+    /**
+     * 該当するbingoRoomのID
+     */
     @NotNull
     private Long bingoRoomId;
 
