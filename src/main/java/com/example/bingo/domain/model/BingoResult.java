c42497f862231b4bd9c489392da33acde9a8b5dc
takuminish <s15t253@stu.kagawa-u.ac.jp>
2020-01-20 22:06:11 +0900

    コメントを追加

--- src/main/java/com/example/bingo/domain/model/BingoResult.java
+++ src/main/java/com/example/bingo/domain/model/BingoResult.java
@@ -12,20 +12,38 @@ import javax.validation.constraints.NotNull;
 
 import lombok.Data;
 
+/**
+ * ビンゴの抽選結果のEntity bingoResultテーブル
+ * 
+ * @author takuminv
+ *
+ */
 @Entity
 @Data
 public class BingoResult {
 
+    /**
+     * 主キーとなるId(自動インクリメント)
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long bingoId;
 
+    /**
+     * 抽選結果の値(0~99)
+     */
     @NotNull
     private String bingoValue;
 
+    /**
+     * 登録日時
+     */
     @NotNull
     private Date createdAt;
 
+    /**
+     * 紐付くBingoRoom
+     */
     @OneToOne
     @JoinColumn(name = "bingoRoomId", referencedColumnName = "bingoRoomId")
     @NotNull
