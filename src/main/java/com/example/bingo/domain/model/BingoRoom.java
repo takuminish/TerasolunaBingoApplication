e7c53fb05b34da413d955ebf3155a6280ac42469
takuminish <s15t253@stu.kagawa-u.ac.jp>
2020-01-20 22:08:18 +0900

    コメントを追加

--- src/main/java/com/example/bingo/domain/model/BingoRoom.java
+++ src/main/java/com/example/bingo/domain/model/BingoRoom.java
@@ -14,6 +14,12 @@ import javax.validation.constraints.NotNull;
 
 import lombok.Data;
 
+/**
+ * ビンゴルームのEntity bingoRoomテーブル
+ * 
+ * @author takuminv
+ *
+ */
 @Entity
 @Data
 @Table(name = "bingoRoom")
@@ -21,25 +27,46 @@ public class BingoRoom implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    /**
+     * 主キーとなるId(自動インクリメント)
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long bingoRoomId;
 
+    /**
+     * ルーム名
+     */
     @NotNull
     private String roomName;
 
+    /**
+     * ゲーム開始済みか
+     */
     @NotNull
     private boolean started;
 
+    /**
+     * ゲーム終了済みか
+     */
     @NotNull
     private boolean finished;
 
+    /**
+     * 登録日時
+     */
     @NotNull
     private Date createdAt;
 
+    /**
+     * 更新日時
+     */
     @NotNull
     private Date updatedAt;
 
+    /**
+     * 紐付くユーザ
+     */
     @OneToOne
     @JoinColumn(name = "createUserId", referencedColumnName = "userId")
     private UserAccount createUserAccount;
